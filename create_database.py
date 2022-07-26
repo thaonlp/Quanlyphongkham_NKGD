08e4d24c8e6984cde8fef268f15a29765823c627
Thao Nguyen <tpnguyen@axon.com>
2022-07-26 11:39:51 +0700

    Update database
    Signed-off-by: Thao Nguyen <tpnguyen@axon.com>

--- create_database.py
+++ create_database.py
@@ -5,20 +5,28 @@ databaseFile = "hsbn.db"
 conn = sqlite3.connect(databaseFile)
 print("Opened database successfully")
 
-conn.execute('''CREATE TABLE DanhSachPhieuKhamBenh 
-       (ID INT PRIMARY KEY     NOT NULL, 
-       NAME           TEXT    NOT NULL, 
-       DOB            INT     NOT NULL, 
+conn.execute('''CREATE TABLE BenhNhan
+       (PATIENTID INT PRIMARY KEY     NOT NULL, 
+       NAME           TEXT    NOT NULL,
+       DOB            INT     NOT NULL,
        ADDRESS        CHAR(100), 
        SEX         CHAR(10),
        JOB CHAR(20),
-       PHONE CHAR(20),
-       CHECKDAY CHAR(20),
-       REASON CHAR(100),
        HISTORY CHAR(100),
        OTHERHIST CHAR(100),
        ALLERGY CHAR(100),
-       PLAN TEXT NOT NULL);''')  
-print("Table created successfully")
+       PHONE CHAR(20));''')
+
+print("Table BenhNhan created successfully")
+
+conn.execute('''CREATE TABLE PhieuKhamBenh
+       (ID INT PRIMARY KEY     NOT NULL,
+       PATIENTID INT NOT NULL,
+       CHECKDAY CHAR(20),
+       REASON CHAR(100),
+       PLAN TEXT NOT NULL,
+       FOREIGN KEY (PATIENTID) REFERENCES BenhNhan (PATIENTID));''')
+
+print("Table PhieuKhamBenh created successfully")
   
 conn.close() 
