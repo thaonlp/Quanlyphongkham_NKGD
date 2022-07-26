06521640e939fa226376e8b8c8e50698f708e59e
Thao Nguyen <tpnguyen@axon.com>
2022-07-26 12:35:01 +0700

    update open database script
    Signed-off-by: Thao Nguyen <tpnguyen@axon.com>

--- open_database.py
+++ open_database.py
@@ -6,7 +6,13 @@ con = sqlite3.connect(databaseFile)
 
 def sql_fetch(con):
     cursorObj = con.cursor()
-    cursorObj.execute('SELECT * FROM DanhSachPhieuKhamBenh')
+    print("Showing table BenhNhan")
+    cursorObj.execute('SELECT * FROM BenhNhan')
+    rows = cursorObj.fetchall()
+    for row in rows:
+        print(row)
+    print("Showing table PhieuKhamBenh")
+    cursorObj.execute('SELECT * FROM PhieuKhamBenh')
     rows = cursorObj.fetchall()
     for row in rows:
         print(row)
