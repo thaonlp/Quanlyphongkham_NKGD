9f571ee147c4c3b5c2213d4c37784405b708ebfe
Thao Nguyen <tpnguyen@axon.com>
2022-07-29 17:06:27 +0700

    Update form
    Signed-off-by: Thao Nguyen <tpnguyen@axon.com>

--- medical_report.py
+++ medical_report.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import sqlite3
 
+from tkinter import messagebox
+
 LEFTSIDE_START = 10
 LABEL_LENGTH = 150
 
@@ -20,10 +22,10 @@ class MedicalReport(Toplevel):
         self.checkDay = StringVar()
         self.address = StringVar()
         self.reason = StringVar()
-        self.history = StringVar()
-        self.otherHistory = StringVar()
-        self.allergy = StringVar()
+        self.note = StringVar()
         self.plan = StringVar()
+
+        self.sex_choice = IntVar()
         self.variable_text_map = {
             "Số hồ sơ": self.id,
             "Họ tên": self.name,
@@ -34,9 +36,7 @@ class MedicalReport(Toplevel):
             "Ngày khám": self.checkDay,
             "Địa chỉ": self.address,
             "Lý do tới khám": self.reason,
-            "Tiền sử bệnh": self.history,
-            "Tiền sử bệnh khác": self.otherHistory,
-            "Tiền sử dị ứng": self.allergy,
+            "Tiền sử bệnh": self.note,
             "Kế hoạch điều trị": self.plan,
         }
         mainlbl=Label(self, text="Phiếu điều trị", fg='red', font=("Helvetica", 28, "bold italic"))
@@ -59,8 +59,6 @@ class MedicalReport(Toplevel):
         self.create_window_label_with_entry("Địa chỉ", LEFTSIDE_START, 200)
         self.create_window_label_with_entry("Lý do tới khám", LEFTSIDE_START, 230)
         self.create_multiple_choices_with_label("Tiền sử bệnh", tienSuBenh, LEFTSIDE_START, 260)
-        self.create_window_label_with_entry("Tiền sử bệnh khác", LEFTSIDE_START, 470)
-        self.create_window_label_with_entry("Tiền sử dị ứng", LEFTSIDE_START, 500)
         self.create_text_box("Kế hoạch điều trị", LEFTSIDE_START, 530)
         # Button
         saveBtn=Button(self, text="Lưu thông tin", fg='green', font=("Times", 15, "bold italic"), command=self.save_Data)
@@ -80,18 +78,26 @@ class MedicalReport(Toplevel):
         txtfld=Entry(self, text=self.variable_text_map[text], bd=3)
         txtfld.place(x=x+400+LABEL_LENGTH,y=y)
 
+    def FM_choice(self):
+        if self.sex_choice.get() == 0:
+            self.sex = "Nam"
+        elif self.sex_choice.get() == 1:
+            self.sex = "Nữ"
+        print("Sex: ", self.sex)
+
     def create_true_false_text_box_with_label(self,label,c1, c2, x,y):
         lbl=Label(self, text=label, fg='blue', font=("Times", 15))
         lbl.place(x=x, y=y)
-        v0=IntVar()
-        v0.set(1)
-        r1=Radiobutton(self, text=c1, variable=v0,value=1)
-        r2=Radiobutton(self, text=c2, variable=v0,value=2)
+
+        self.sex_choice.set(1)
+        r1=Radiobutton(self, text=c1, variable=self.sex_choice,value=0, command=self.FM_choice)
+        r2=Radiobutton(self, text=c2, variable=self.sex_choice,value=1, command=self.FM_choice)
         r1.place(x=x + LABEL_LENGTH ,y=y)
         r2.place(x=x + LABEL_LENGTH + 60, y=y)
 
+
     def create_choice(self,label,x,y):
-        choice = Checkbutton(self, text=label, variable = label)
+        choice = Checkbutton(self, text=label, variable = self.note)
         choice.place(x=x, y = y)
 
     def create_multiple_choices_with_label(self, mainLable, list_of_choices, x, y):
@@ -114,7 +120,9 @@ class MedicalReport(Toplevel):
 
     def sql_insert_BenhNhan(self, con, entities):
         cursorObj = con.cursor()
-        cursorObj.execute('INSERT INTO BenhNhan(PATIENTID,NAME,SEX,DOB,JOB,PHONE,ADDRESS,REASON,HISTORY,OTHERHIST,ALLERGY) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', entities)
+        #  Table columns of BenhNhan ['index', 'ID', 'FIRSTNAME', ' FULLNAME', 'BIRTH', 'ADDRESS', 'NOTE', 'SEX', 'JOB', 'PHONE']
+        cursorObj.execute('INSERT INTO BenhNhan(ID,FIRSTNAME, FULLNAME,BIRTH, ADDRESS, NOTE, SEX, JOB, PHONE) \
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', entities)
         con.commit()
 
     def sql_insert_PhieuKhamBenh(self, con, entities):
@@ -125,14 +133,17 @@ class MedicalReport(Toplevel):
     def save_Data(self):
         conn = sqlite3.connect(databaseFile)
         print("Opened database successfully")
-        d = lambda: [self.variable_text_map[data].get() for data in self.variable_text_map]
-        print(d())
-        for data in self.variable_text_map:
-            content = self.variable_text_map[data].get()
-            print(content)
+        #d = lambda: [self.variable_text_map[data].get() for data in self.variable_text_map]
+        #print(d())
+        #for data in self.variable_text_map:
+        #    content = self.variable_text_map[data].get()
+        #    print(content)
         self.text_box.get(1.0, END)
-        self.sql_insert(conn, d())
+
+
         print("Data saved!")
+        messagebox.showinfo("Tạo bệnh nhân mới", "Đã lưu thông tin")
+
         conn.close()
 
     def close_window(self):
